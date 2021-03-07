d5426cb43c465b192af7bc033bac1573f4a2bc5b
sadeghrafiei <74002994+sadeghrafiei@users.noreply.github.com>
2021-03-07 18:31:30 +0330

    add material ui icons

--- src/components/Preview/Preview.js
+++ src/components/Preview/Preview.js
@@ -1,21 +1,31 @@
 import React, { useEffect } from "react";
-import { useSelector , useDispatch } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import { useHistory } from "react-router-dom";
-import { resetCameraImage, selectCameraImage } from "../../features/cameraSlice";
+import {
+  resetCameraImage,
+  selectCameraImage,
+} from "../../features/cameraSlice";
 import "./Preview.css";
 
-import CloseIcon from '@material-ui/icons/Close';
+import CloseIcon from "@material-ui/icons/Close";
+import TextFieldsIcon from "@material-ui/icons/TextFields";
+import CreateIcon from "@material-ui/icons/Create";
+import NoteIcon from "@material-ui/icons/Note";
+import MusicNoteIcon from "@material-ui/icons/MusicNote";
+import AttachFileIcon from "@material-ui/icons/AttachFile";
+import CropIcon from "@material-ui/icons/Crop";
+import TimerIcon from "@material-ui/icons/Timer";
 
 function Preview() {
   const cameraImage = useSelector(selectCameraImage);
   const history = useHistory();
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
 
   useEffect(() => {
     if (!cameraImage) {
-        history.replace('/')
-    }   
-  }, [cameraImage , history]);
+      history.replace("/");
+    }
+  }, [cameraImage, history]);
 
   const closePreview = () => {
     dispatch(resetCameraImage());
@@ -23,7 +33,16 @@ function Preview() {
 
   return (
     <div className="preview">
-      <CloseIcon onClick={closePreview} className="preview__close" />  
+      <CloseIcon onClick={closePreview} className="preview__close" />
+      <div className="preview__toolbarRight">
+        <TextFieldsIcon />
+        <CreateIcon />
+        <NoteIcon />
+        <MusicNoteIcon />
+        <AttachFileIcon />
+        <CropIcon />
+        <TimerIcon />
+      </div>
       <img src={cameraImage} alt="" />
     </div>
   );
