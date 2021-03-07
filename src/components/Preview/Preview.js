f3c5de9a73170034f3fe8a7229e7fd599444527d
sadeghrafiei <74002994+sadeghrafiei@users.noreply.github.com>
2021-03-07 17:54:43 +0330

    add style

--- src/components/Preview/Preview.js
+++ src/components/Preview/Preview.js
@@ -1,12 +1,32 @@
-import React from 'react';
-import "./Preview.css"
+import React, { useEffect } from "react";
+import { useSelector , useDispatch } from "react-redux";
+import { useHistory } from "react-router-dom";
+import { resetCameraImage, selectCameraImage } from "../../features/cameraSlice";
+import "./Preview.css";
+
+import CloseIcon from '@material-ui/icons/Close';
 
 function Preview() {
-    return (
-        <div className="preview">
-            
-        </div>
-    )
+  const cameraImage = useSelector(selectCameraImage);
+  const history = useHistory();
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    if (!cameraImage) {
+        history.replace('/')
+    }   
+  }, [cameraImage , history]);
+
+  const closePreview = () => {
+    dispatch(resetCameraImage());
+  };
+
+  return (
+    <div className="preview">
+      <CloseIcon onClick={closePreview} className="preview__close" />  
+      <img src={cameraImage} alt="" />
+    </div>
+  );
 }
 
-export default Preview
+export default Preview;
