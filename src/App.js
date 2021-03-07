4a5cb09e4c61535b1cfdddc692c84b82b9f4388e
sadeghrafiei <74002994+sadeghrafiei@users.noreply.github.com>
2021-03-07 14:21:47 +0330

    add camera

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import WebcamCapture from './components/webcam/WebcamCapture';
 
 function App() {
   return (
     <div className="App">
       <h1>LETS BUILD SNAPCHAT</h1>
+      <WebcamCapture />
     </div>
   );
 }
