ca4c77dec87a9f9319801667099618e3b2a3ccb0
sadeghrafiei <74002994+sadeghrafiei@users.noreply.github.com>
2021-03-07 18:54:23 +0330

    add full style at preview

--- src/components/Preview/Preview.js
+++ src/components/Preview/Preview.js
@@ -15,6 +15,7 @@ import MusicNoteIcon from "@material-ui/icons/MusicNote";
 import AttachFileIcon from "@material-ui/icons/AttachFile";
 import CropIcon from "@material-ui/icons/Crop";
 import TimerIcon from "@material-ui/icons/Timer";
+import SendIcon from '@material-ui/icons/Send'
 
 function Preview() {
   const cameraImage = useSelector(selectCameraImage);
@@ -44,6 +45,10 @@ function Preview() {
         <TimerIcon />
       </div>
       <img src={cameraImage} alt="" />
+      <div className="preview__footer">
+          <h2>Send Now</h2>
+          <SendIcon fontSize="small" className="preview__sendIcon" />
+      </div>
     </div>
   );
 }
